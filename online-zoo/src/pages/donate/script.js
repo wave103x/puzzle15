80231951e4f8893b4f1d561bf63bd5b43daf271a
Mikhail Sprygin <wave103x@gmail.com>
2022-10-06 18:00:36 +0300

    feat: add menu, testimonials popup, donate page tasks

--- online-zoo/src/pages/donate/script.js
+++ online-zoo/src/pages/donate/script.js
@@ -1,3 +1,60 @@
 import 'normalize.css'
 import './style.css'
 import '../../assets/styles/general.css'
+
+
+//      burger menu
+
+const burgerIcon = document.querySelector('.header__burger');
+const burgerMenu = document.querySelector('.burger-menu');
+const overlay = document.querySelector('.overlay_menu');
+
+burgerIcon.addEventListener('click', (event) => {
+    burgerMenu.classList.toggle('burger-menu_active');
+    overlay.classList.toggle('overlay_active')
+
+    if (burgerIcon.firstElementChild.src.includes('burger.svg')) {
+        burgerIcon.firstElementChild.src = './assets/close.svg';
+    } else {
+        burgerIcon.firstElementChild.src = './assets/burger.svg';
+    }
+});
+
+document.addEventListener('click', (event) => {
+
+    if (event.target === overlay) {
+
+        burgerMenu.classList.toggle('burger-menu_active');
+        overlay.classList.toggle('overlay_active');
+
+        if (burgerIcon.firstElementChild.src.includes('burger.svg')) {
+            burgerIcon.firstElementChild.src = './assets/close.svg';
+        } else {
+            burgerIcon.firstElementChild.src = './assets/burger.svg';
+        }
+    }
+})
+
+
+//      amount
+
+const anotherAmountInput = document.querySelector('.input-text_number');
+const radiosAmount = document.querySelector('.donate-form__amount');
+const radioBtnsAmount = document.querySelectorAll('.donate-amount__item input');
+
+radiosAmount.addEventListener('click', (event) => {
+    event.target.previousElementSibling.checked = true;
+    event.preventDefault();
+    anotherAmountInput.value = +event.target.textContent.slice(1);
+})
+
+
+anotherAmountInput.addEventListener('input', () => {
+    if (anotherAmountInput.value.length > 4) {
+        anotherAmountInput.value = anotherAmountInput.value.slice(0,4);
+    }
+
+    let choosenElement = Array.from(radioBtnsAmount).find(e => +e.value === +anotherAmountInput.value);
+    choosenElement.checked = true;
+    anotherAmountInput.value = choosenElement.value;
+})
--- online-zoo/src/pages/main/script.js
+++ online-zoo/src/pages/main/script.js
@@ -1,3 +1,5 @@
+'use strict'
+
 import 'normalize.css'
 import './style.css'
 import '../../assets/styles/general.css'
@@ -25,6 +27,63 @@ import '../../assets/icons/instagram.svg'
 import '../../assets/icons/twitter.svg'
 import '../../assets/icons/youtube.svg'
 import '../../assets/icons/meet-fish_icon.svg'
+import '../../assets/icons/burger.svg'
+import '../../assets/icons/close.svg'
+import '../../assets/icons/x_icon.svg'
 
-console.log('Отступ слева в футтере на 1000px - ошибка макета, за это не снижается, написано в задании.')
-console.log('Внутри среднего отзыва также ошибка макета.')
+//      burger menu
+
+const burgerIcon = document.querySelector('.header__burger');
+const burgerMenu = document.querySelector('.burger-menu');
+const overlay = document.querySelector('.overlay_menu');
+
+burgerIcon.addEventListener('click', (event) => {
+    burgerMenu.classList.toggle('burger-menu_active');
+    overlay.classList.toggle('overlay_active')
+
+    if (burgerIcon.firstElementChild.src.includes('burger.svg')) {
+        burgerIcon.firstElementChild.src = './assets/close.svg';
+    } else {
+        burgerIcon.firstElementChild.src = './assets/burger.svg';
+    }
+});
+
+document.addEventListener('click', (event) => {
+
+    if (event.target === overlay) {
+
+        burgerMenu.classList.toggle('burger-menu_active');
+        overlay.classList.toggle('overlay_active');
+
+        if (burgerIcon.firstElementChild.src.includes('burger.svg')) {
+            burgerIcon.firstElementChild.src = './assets/close.svg';
+        } else {
+            burgerIcon.firstElementChild.src = './assets/burger.svg';
+        }
+    }
+})
+
+
+
+// testimonials pop-up
+
+const testimonials = document.querySelector('.testimonials__layout');
+const overylayTestimonials = document.querySelector('.overlay_testimonials')
+
+testimonials.addEventListener('click', (event) => {
+    let target = event.target.closest('.testimonials-layout__item');
+    if (!target) return;
+    overylayTestimonials.classList.toggle('overlay_active');
+
+    let testimonial = target.cloneNode(true);
+    testimonial.classList.add('testimonial_popup');
+    overylayTestimonials.append(testimonial)
+})
+
+overylayTestimonials.addEventListener('click', (event) => {
+    let testimonial = document.querySelector('.testimonial_popup');
+    if (!event.target.closest('.testimonial_popup')) {
+        testimonial.remove();
+        overylayTestimonials.classList.toggle('overlay_active');
+    }
+})
